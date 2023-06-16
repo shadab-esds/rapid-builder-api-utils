b8b79734348b1b08551d2040f9e987382e98f424
shadab.khan <shadab.khan@esds.co.in>
2023-06-16 19:36:50 +0530

    first commit

---
+++ src/rapidbuilder_api_utils/__init__.py
@@ -0,0 +1,9 @@
+"""The Rapid Builder API service."""
+
+from rapidbuilder_api_utils.utils.startup import (
+    collect_role_ids,
+    setup_jwt_manager,
+    collect_user_resource_ids,
+)
+
+__version__ = "5.0.1"
---
+++ src/rapidbuilder_api_utils/schemas/__init__.py
@@ -0,0 +1,2 @@
+"""This exports all of the schemas used by the application."""
+from rapidbuilder_api_utils.schemas.formio_roles import FormioRoleSchema
