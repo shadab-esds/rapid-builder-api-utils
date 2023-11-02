3be70af18fab2fa3e143c5a18964c999917db240
shadab.khan <shadab.khan@esds.co.in>
2023-11-02 13:51:35 +0530

    WIP

---
+++ src/rapidbuilder_api_utils/utils/roles.py
@@ -0,0 +1,9 @@
+"""Role definitions."""
+
+
+# class Role:  # pylint: disable=too-few-public-methods
+#     """User Role."""
+
+#     rapidbuilder_designer = "rapidbuilder-designer"
+#     rapidbuilder_reviewer = "rapidbuilder-reviewer"
+#     rapidbuilder_client = "rapidbuilder-client"
